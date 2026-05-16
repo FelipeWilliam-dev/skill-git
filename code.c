0d3bd033060a353cfb07418f4b1427e531b82757
Felipe <felipe.silva@ufam.edu.br>
2026-05-16 12:09:52 -0400

    :recycle: refactor: Otimizando o laço de repetição para imprimir números pares

--- code.c
+++ code.c
@@ -6,11 +6,10 @@ int main() {
     printf("Contando numeros pares ate %d:\n", limite);
 
     // Loop que passa por todos os numeros de 1 a 10
-    for (int i = 1; i <= limite; i++) {
-        // Verifica se o numero e par
-        if (i % 2 == 0) {
-            printf("%d\n", i);
-        }
+    for (int i = 0; i <= limite; i+=2) {
+        
+        printf("%d\n", i);
+    
     }
 
     return 0;
