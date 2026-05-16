7499c2b0f9bc60e88cf28bdacd7f4814f8cab4bd
Felipe <felipe.silva@ufam.edu.br>
2026-05-16 16:09:49 -0400

    :sparkles: feat: adicionando funcionalidade de contar

--- code.c
+++ code.c
@@ -6,7 +6,7 @@ int main() {
     printf("Contando numeros pares ate %d:\n", limite);
 
     // Loop que passa por todos os numeros de 1 a 10
-    for (int i = 0; i <= limite; i+=2) {
+    for (int i = 0; i <= limite; i++) {
         
         printf("%d\n", i);
     
